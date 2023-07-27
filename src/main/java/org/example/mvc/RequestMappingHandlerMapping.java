5c2c0a96b355ff298404dfba198404793b94ac74
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-27 13:24:16 +0900

    add UserListController

--- src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -2,6 +2,7 @@ package org.example.mvc;
 
 import org.example.mvc.controller.Controller;
 import org.example.mvc.controller.HomeController;
+import org.example.mvc.controller.UserListController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ public class RequestMappingHandlerMapping {
 
     public void init() {
         mappings.put("/", new HomeController());
+        mappings.put("/users", new UserListController());
     }
 
     public Controller findHandler(String uriPath) {
